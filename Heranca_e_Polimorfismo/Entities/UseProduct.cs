4cfeb799ab11b6d03f9542953228dd99638b623c
Denis James <dnsmarangonee@gmail.comgit config --global user.email dnsmarangonee@gmail.com>
2023-02-28 16:19:59 -0300

    Herenca mais polimorfismo, produtos importados e usados

---
+++ Heranca_e_Polimorfismo/Entities/UseProduct.cs
@@ -0,0 +1,18 @@
+﻿
+
+namespace Heranca_e_Polimorfismo.Entities
+{
+    internal class UseProduct :Product
+    {
+        public DateTime ManufactureDate { get; set; }
+        public UseProduct() { }
+        public UseProduct(string name, double price, DateTime manuFactureDate):base(name, price)
+        {
+            ManufactureDate = manuFactureDate;
+        }
+        public override string PriceTag()
+        {
+            return Name +" (used) $"+Price.ToString("f2")+" (Manufacture date: "+ManufactureDate+")";
+        }
+    }
+}
---
+++ Heranca_e_Polimorfismo/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Globalization;
+using Heranca_e_Polimorfismo.Entities;
+
+namespace Heranca_e_Polimorfismo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> prod = new List<Product>();
+            Console.Write("Enter the number of products: ");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                Console.Write(" Comm, used or imported (c/u/i): ");
+                char r = char.Parse(Console.ReadLine());
+                if (r == 'c' || r == 'C')
+                {
+                    Console.Write("Name: ");
+                    string name = Console.ReadLine();
+                    Console.Write("Price: ");
+                    double price = double.Parse(Console.ReadLine());
+                    prod.Add(new Product(name, price));
+                }
+                else if (r == 'i' || r == 'I')
+                {
+                    Console.Write("Name: ");
+                    string name = Console.ReadLine();
+                    Console.Write("Price: ");
+                    double price = double.Parse(Console.ReadLine());
+                    Console.Write("Customs free: ");
+                    double customs = double.Parse(Console.ReadLine());
+                    prod.Add(new ImportedProduct(name, price, customs));
+                }
+                else
+                {
+                    Console.Write("Name: ");
+                    string name = Console.ReadLine();
+                    Console.Write("Price: ");
+                    double price = double.Parse(Console.ReadLine());
+                    Console.Write("Manudacture (MM/DD/YYYY): ");
+                    DateTime date = DateTime.Parse(Console.ReadLine());
+                    prod.Add(new UseProduct(name, price, date));
+                }
+                Console.WriteLine();
+                Console.WriteLine("Price Tags: ");
+                foreach(Product pro in prod)
+                {
+                    Console.WriteLine(pro.PriceTag());
+                }
+
+            }
+        }
+    }
+}
+
+
